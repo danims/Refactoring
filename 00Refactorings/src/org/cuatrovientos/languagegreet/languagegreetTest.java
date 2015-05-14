925ae332cf0425bd81d61a97f789040287c62c04
PC <PC@PCDAM.4VIENTOSDAM6.LOCAL>
2015-05-14 10:05:03 +0200

    Third

--- 00Refactorings/src/org/cuatrovientos/languagegreet/languagegreetTest.java
+++ 00Refactorings/src/org/cuatrovientos/languagegreet/languagegreetTest.java
@@ -16,15 +16,15 @@ public class languagegreetTest {
 		languagegreet myGreet = new languagegreet();
 
 		String expected = "hola";
-		String target = myGreet.greet_method(0, 1);
+		String target = myGreet.greet(0, 1);
 		assertEquals("Testing spanish greet once", expected, target);
 
 		expected = "hellohello";
-		target = myGreet.greet_method(1, 2);
+		target = myGreet.greet(1, 2);
 		assertEquals("Testing english greet twice", expected, target);
 
 		expected = "kaixokaixokaixokaixo";
-		target = myGreet.greet_method(2, 4);
+		target = myGreet.greet(2, 4);
 		assertEquals("Testing basque greet four times", expected, target);
 
 	}
